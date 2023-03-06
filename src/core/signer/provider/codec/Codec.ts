c2abcb05689c0fd68a48454b16e120a93d7756c4
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-03-06 19:54:49 +0200

    Move filename calcuation to Codec interface

--- src/core/signer/provider/codec/Codec.ts
+++ src/core/signer/provider/codec/Codec.ts
@@ -9,4 +9,11 @@ export interface Codec {
      * @param buffer Raw data provided by signer provider
      */
     loadPrivateKey(identifier: string, buffer: Uint8Array): Promise<Uint8Array>;
+
+    /**
+     * Determines file name from identifier
+     *
+     * @param identifier Identifier used to request the key
+     */
+    determineFilename(identifier: string): Promise<string>;
 }
