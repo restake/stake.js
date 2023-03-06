e2f8bf72a0199f625e08a82c20d6d049b71062eb
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-03-06 19:04:14 +0200

    midlevel -> core

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,6 +1,6 @@
-import { NEARProtocol, NEARSigner, networks, ntoy } from "@restake/staking-sdk/midlevel/protocol/near";
-import { ed25519PrivateKey, ed25519Signer } from "@restake/staking-sdk/midlevel/signer";
-import { FilesystemSignerProvider, FireblocksSignerProvider } from "@restake/staking-sdk/midlevel/signer/provider";
+import { NEARProtocol, NEARSigner, networks, ntoy } from "@restake/staking-sdk/core/protocol/near";
+import { ed25519PrivateKey, ed25519Signer } from "@restake/staking-sdk/core/signer";
+import { FilesystemSignerProvider, FireblocksSignerProvider } from "@restake/staking-sdk/core/signer/provider";
 
 const provider = new FilesystemSignerProvider<ed25519Signer>("/Users/mark/.near-credentials/testnet", (identifier, bytes) => {
     const privateKey = new ed25519PrivateKey(bytes);
