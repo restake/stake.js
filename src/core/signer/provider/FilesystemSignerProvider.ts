bf27665c17b484d1df28889e83857fec2395a955
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-03-06 19:48:48 +0200

    Add codec interface for obtaining a private key

--- src/core/signer/provider/FilesystemSignerProvider.ts
+++ src/core/signer/provider/FilesystemSignerProvider.ts
@@ -1,6 +1,7 @@
+import { Codec } from "./codec/Codec.js";
+import { JSONHexEncodedKeyCodec } from "./codec/JSONHexEncodedKeyCodec.js";
 import { SignerProvider } from "./provider.js";
 
-import { hexToBytes } from "@noble/curves/abstract/utils";
 import { readFile } from "node:fs/promises";
 import { basename, join } from "node:path";
 
@@ -8,20 +9,20 @@ export type SignerConstructor<S> = (identifier: string, bytes: Uint8Array) => S;
 
 export class FilesystemSignerProvider<S> implements SignerProvider<S> {
     #keyDirectory: string;
-    #constructorFunc: SignerConstructor<S>
+    #constructorFunc: SignerConstructor<S>;
+    #codec: Codec;
 
-    constructor(keyDirectory: string, constructorFunc: SignerConstructor<S>) {
+    constructor(keyDirectory: string, constructorFunc: SignerConstructor<S>, codec: Codec = JSONHexEncodedKeyCodec) {
         this.#keyDirectory = keyDirectory;
         this.#constructorFunc = constructorFunc;
+        this.#codec = codec;
     }
 
     async getSigner(identifier: string): Promise<S> {
         const filePath = join(this.#keyDirectory, basename(identifier + ".json"));
 
-        const file = await readFile(filePath, { encoding: "utf8" });
-        const parsed = JSON.parse(file) as { privateKey: string };
-
-        const decoded = hexToBytes(parsed.privateKey);
+        const file = await readFile(filePath);
+        const decoded = await this.#codec.loadPrivateKey(identifier, file);
         const signer = this.#constructorFunc(identifier, decoded);
 
         return signer;
---
+++ src/core/signer/provider/codec/Codec.ts
@@ -0,0 +1,12 @@
+/**
+ * Interface to obtain a private key from the buffer
+ */
+export interface Codec {
+    /**
+     * Loads private key from buffer
+     *
+     * @param identifier Identifier used to request the key
+     * @param buffer Raw data provided by signer provider
+     */
+    loadPrivateKey(identifier: string, buffer: Uint8Array): Promise<Uint8Array>;
+}
