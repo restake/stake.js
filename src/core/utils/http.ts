8214fcc4a50aa63cb0f5cb99d292fa52f4a3fab5
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-03-10 14:25:54 +0200

    Create initial NEAR broadcast implementation

---
+++ src/core/utils/http.ts
@@ -0,0 +1,55 @@
+/*
+export class HTTPError implements Error {
+    name: string;
+    message: string;
+    stack?: string | undefined;
+    cause?: unknown;
+}
+*/
+
+export type JSONRPCRequest = {
+    jsonrpc: string;
+    id: string | number;
+    method: string;
+    params: unknown;
+};
+
+export type JSONRPCResponse<T, E> = {
+    jsonrpc: string;
+    id: string | number;
+    result: T;
+    error: E;
+};
+
+export async function jsonrpc<T>(endpoint: URL, method: string, params: unknown): Promise<T> {
+    const body: JSONRPCRequest = {
+        jsonrpc: "2.0",
+        id: "123",
+        method,
+        params,
+    };
+
+    const request = await fetch(endpoint, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    });
+
+    // TODO: brittle
+    const contentType = request.headers.get("Content-Type");
+    if (!contentType?.toLowerCase()?.startsWith("application/json")) {
+        // throw new HTTPError();
+    }
+
+    const responseBody = await request.json() as JSONRPCResponse<T, unknown>;
+    if (request.status !== 200) {
+        // throw new HTTPError();
+    }
+
+    // TODO: proper error handling
+
+    return responseBody.result;
+}
