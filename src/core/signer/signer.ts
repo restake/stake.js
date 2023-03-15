06cf27923e9e3ea353e1731885531ef8efa47812
Hans Oskar Aaviksoo <ha@restake.net>
2023-03-15 17:02:01 +0200

    Add JSDoc comments for NEAR protocol

--- src/core/signer/signer.ts
+++ src/core/signer/signer.ts
@@ -1,5 +1,18 @@
 export interface Signer<P = Uint8Array, S = Uint8Array> {
+    /**
+     * Signs provided payload
+     *
+     * @param payload Data to sign
+     * @returns Signature
+     */
     sign(payload: P): Promise<S>;
 
+    /**
+     * Verifies whether signature is valid
+     *
+     * @param payload Payload with a signature
+     * @param signature Signature to verify
+     * @returns Boolean
+     */
     verify(payload: P, signature: S): Promise<boolean>;
 }
