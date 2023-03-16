7a087e7127603786b6a92d8d23fa4eecc20bf2ce
Hans Oskar Aaviksoo <ha@restake.net>
2023-03-16 17:58:42 +0200

    Add Ethereum protocol implementation for current structure

--- src/core/protocol/ethereum/EthereumProtocol.ts
+++ src/core/protocol/ethereum/EthereumProtocol.ts
@@ -1,57 +1,81 @@
 import Web3 from "web3";
 import { Account } from "web3-core";
 import { AbiItem } from "web3-utils";
-import web3eth from "web3-eth";
+import { SignedTransaction, Transaction } from "./EthereumTransaction.js";
+import { TransactionBroadcaster } from "../../network/broadcaster.js";
+
 
 const web3 = new Web3("HTTP://127.0.0.1:7545");
 
-export async function stake(){
-  const eth = new web3eth.Eth(web3.currentProvider);
-  const chainId = await eth.getChainId();
-  const gasPrice = await eth.getGasPrice();
+export type EthereumBroadcastResponse = {
+    txID: string;
+};
 
-  /*
-  const contractAddress = "0x00000000219ab540356cBB839Cbe05303d7705Fa";
-  const myContractAbi: AbiItem[] = [
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "token",
-          type: "address"
-        }
-      ],
-      name: "deposit",
-      outputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256"
-        }
-      ],
-      stateMutability: "payable",
-      type: "function"
+//export async function stake(account: Account, validatorPublicKey: string, amount: number) {
+
+export class EthereumProtocol implements TransactionBroadcaster<SignedTransaction, EthereumBroadcastResponse> {
+    static INSTANCE = new EthereumProtocol();
+
+    private constructor() {
+        // no-op
     }
-  ];
 
-  const contract = new web3.eth.Contract(myContractAbi, contractAddress);
+    async stake(
+        signer: EthereumSigner,
+        account: Account,
+        validatorPublicKey: string,
+        amount: number,
+    ): Promise<Transaction> {
+        const chainId = await web3.eth.getChainId();
 
-  const tx = {
-    from: account.address,
-    to: contractAddress,
-    value: web3.utils.toWei(amount.toString(), "ether"),
-    data: contract.methods.deposit(validatorPublicKey).encodeABI(),
-    chainId,
-    gasPrice,
-    gas: 200000
-  };
+        const gasPrice = await web3.eth.getGasPrice();
 
-  const signedTx = await web3.eth.accounts.signTransaction(tx, account.privateKey);
+        const contractAddress = "0x00000000219ab540356cBB839Cbe05303d7705Fa";
+        const myContractAbi: AbiItem[] = [
+            {
+            inputs: [
+                {
+                internalType: "address",
+                name: "token",
+                type: "address"
+                }
+            ],
+            name: "deposit",
+            outputs: [
+                {
+                internalType: "uint256",
+                name: "",
+                type: "uint256"
+                }
+            ],
+            stateMutability: "payable",
+            type: "function"
+            }
+        ];
 
-  const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction!);
+        const contract = new web3.eth.Contract(myContractAbi, contractAddress);
 
-  return receipt.transactionHash;
+        const tx = {
+            from: account.address,
+            to: contractAddress,
+            value: web3.utils.toWei(amount.toString(), "ether"),
+            data: contract.methods.deposit(validatorPublicKey).encodeABI(),
+            chainId,
+            gasPrice,
+            gas: 200000
+        };
 
-  */
- return chainId;
+        return {
+            payload: tx,
+            network: signer.network,
+        };
+    };
+
+    async broadcast(signedTransaction: SignedTransaction): Promise<EthereumBroadcastResponse> {
+        throw new Error("");
+    }
+
+    async broadcastSimple(signedTransaction: SignedTransaction): Promise<string> {
+        throw new Error("");
+    }
 }
