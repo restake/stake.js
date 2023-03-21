8b687be667b7506a7ed094ac008383dcb169ad54
Hans Oskar Aaviksoo <ha@restake.net>
2023-03-21 19:36:53 +0200

    Remove unnecessary dependencies including web3

--- src/core/protocol/ethereum/EthereumProtocol.ts
+++ src/core/protocol/ethereum/EthereumProtocol.ts
@@ -1,65 +1,13 @@
-import Web3 from "web3";
-import { Account } from "web3-core";
-import { AbiItem } from "web3-utils";
-import { TransactionConfig } from "web3-core";
-//import { SignedTransaction, Transaction } from "./EthereumTransaction.js";
-import { SignedTransaction } from "./EthereumTransaction.js";
+import { jsonrpc } from "../../utils/http.js";
+import { EthereumSigner } from "./EthereumSigner.js";
+import { SignedTransaction, Transaction } from "./EthereumTransaction.js";
 import { TransactionBroadcaster } from "../../network/broadcaster.js";
-import { Transaction } from "@ethereumjs/tx";
-//import CustomCommon from "ethereumjs-common";
-import {Chain, Common, Hardfork}  from "@ethereumjs/common";
+import { Transaction as EthTransaction } from "@ethereumjs/tx";
+import { Common }  from "@ethereumjs/common";
+import { encode as b64Encode, decode as b64Decode } from "../../utils/base64.js";
 
 
-//const web3 = new Web3("HTTP://127.0.0.1:7545");
-
-
-export type EthereumBroadcastResponse = {
-    txID: string;
-};
-
-//export async function stake(account: Account, validatorPublicKey: string, amount: number) {
-
-export async function transfer(PrivateKey: string, receiveAddress: string, amount: number, senderAddress: string){
-
-    const web3 = new Web3("HTTP://127.0.0.1:7545");
-    const privateKey = Buffer.from(PrivateKey, 'hex');
-    const gasPrice = await web3.eth.getGasPrice();
-    const block = await web3.eth.getBlock("latest");
-    const nonce = await web3.eth.getTransactionCount(senderAddress);
-    const value = web3.utils.toWei(amount.toString(), "ether");
-    const chainId = await web3.eth.getChainId();
-
-    const chainParams = {
-        name: 'testnetwork',
-        networkId: chainId,
-        chainId: chainId,
-        url: "HTTP://127.0.0.1:7545",
-        comment: "Test Local Chain"
-    };
-
-    const customCommon = Common.custom(chainParams);
-
-    const txParams = {
-        nonce: web3.utils.toHex(nonce),
-        gasPrice: web3.utils.toHex(gasPrice),
-        gasLimit: web3.utils.toHex(block.gasLimit),
-        to: receiveAddress,
-        value: web3.utils.toHex(value),
-    }
-
-    const tx = Transaction.fromTxData(txParams, { common: customCommon });
-    // tx signing should be done with noble/curves (secp256k1)
-
-    const signedTx = tx.sign(privateKey);
-    console.log(tx);
-    console.log("NEW LINE \n \n");
-    console.log(signedTx);
-    const serializedTx = signedTx.serialize();
-
-    const receipt = await web3.eth.sendSignedTransaction("0x" + serializedTx.toString("hex"));
-
-    console.log(`Transaction successful. Transaction hash: ${receipt.transactionHash}`);
-};
+export type EthereumBroadcastResponse = string;
 
 export class EthereumProtocol implements TransactionBroadcaster<SignedTransaction, EthereumBroadcastResponse> {
     static INSTANCE = new EthereumProtocol();
@@ -68,71 +16,65 @@ export class EthereumProtocol implements TransactionBroadcaster<SignedTransactio
         // no-op
     }
 
-
-    /*
-    async stake(
+    async transfer(
         signer: EthereumSigner,
-        account: Account,
-        validatorPublicKey: string,
-        amount: number,
-    ): Promise<Transaction> {
-        const chainId = await web3.eth.getChainId();
+        receiveAddress: string,
+        amount: BigInt,
+    ): Promise<Transaction>{
+        const senderAddress = await signer.getAddress();
+        const gasPrice = await signer.fetchGasPrice();
+        const block = await signer.fetchBlockHash("latest");
+        const nonce = await signer.fetchNonce(senderAddress);
+        const chainId = signer.network.chainId;
 
-        const chainId2 = await web3.eth.signTransaction();
-
-        const gasPrice = await web3.eth.getGasPrice();
-
-        const contractAddress = "0x00000000219ab540356cBB839Cbe05303d7705Fa";
-
-        const myContractAbi: AbiItem[] = [
-            {
-            inputs: [
-                {
-                internalType: "address",
-                name: "token",
-                type: "address"
-                }
-            ],
-            name: "deposit",
-            outputs: [
-                {
-                internalType: "uint256",
-                name: "",
-                type: "uint256"
-                }
-            ],
-            stateMutability: "payable",
-            type: "function"
-            }
-        ];
-
-        const contract = new web3.eth.Contract(myContractAbi, contractAddress);
-
-        const tx = {
-            from: account.address,
-            to: contractAddress,
-            value: web3.utils.toWei(amount.toString(), "ether"),
-            data: contract.methods.deposit(validatorPublicKey).encodeABI(),
-            chainId,
-            gasPrice,
-            gas: 200000
+        const chainParams = {
+            name: 'testnetwork',
+            networkId: chainId,
+            chainId: chainId,
+            url: signer.network.rpcUrl,
+            comment: "Test Local Chain"
         };
 
+        const customCommon = Common.custom(chainParams);
+
+        const txParams = {
+            nonce: "0x" + nonce.toString(16),
+            gasPrice: "0x" + gasPrice.toString(16),
+            gasLimit: block.gasLimit,
+            to: receiveAddress,
+            value: "0x" + amount.toString(16),
+        }
+
+        const payload = EthTransaction.fromTxData(txParams, { common: customCommon });
+
         return {
-            payload: tx,
+            payload,
             network: signer.network,
         };
     };
-    */
 
+
+
+    /**
+     * Broadcast the transaction using sendRawTransaction JSON-RPC method.
+     * For more information: https://ethereum.org/en/developers/docs/apis/json-rpc/#eth_sendrawtransaction
+     *
+     * @param signedTransaction
+     * @returns EthereumBroadcastResponse
+     */
     async broadcast(signedTransaction: SignedTransaction): Promise<EthereumBroadcastResponse> {
-        throw new Error("");
+        const endpoint = new URL(signedTransaction.transaction.network.rpcUrl);
+        const serializedTx = signedTransaction.payload.serialize();
+
+        const response = await jsonrpc<EthereumBroadcastResponse>(endpoint, "eth_sendRawTransaction", [
+            "0x" + serializedTx.toString("hex")
+        ]);
+
+        return response;
     }
 
     async broadcastSimple(signedTransaction: SignedTransaction): Promise<string> {
-        throw new Error("");
+        const response = await this.broadcast(signedTransaction);
+        return response;
     }
-
-
-
 }
--- src/core/protocol/ethereum/index.ts
+++ src/core/protocol/ethereum/index.ts
@@ -1,1 +1,4 @@
 export * from "./EthereumProtocol.js";
+export * from "./EthereumSigner.js";
+export * from "./EthereumTransaction.js";
+export * from "./network.js";
