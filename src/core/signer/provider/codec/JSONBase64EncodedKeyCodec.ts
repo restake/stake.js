c79c75f4cb9a1cbd336e42b627c8ce5ba79755fe
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-03-29 17:06:10 +0300

    Create JSON base64 encoded key codec

---
+++ src/core/signer/provider/codec/JSONBase64EncodedKeyCodec.ts
@@ -0,0 +1,21 @@
+import { Codec } from "./Codec.js";
+
+import { basename } from "node:path";
+import { decode as b64decode } from "../../../utils/base64.js";
+
+/**
+ * Codec which handles reading JSON object with key `privateKey` containing base64 encoded private key bytes
+ */
+export const JSONBase64EncodedKeyCodec: Codec = {
+    async loadPrivateKey(_identifier: string, buffer: Uint8Array): Promise<Uint8Array> {
+        const bytes = new TextDecoder().decode(buffer);
+
+        const parsed = JSON.parse(bytes) as { privateKey: string };
+        const decoded = b64decode(parsed.privateKey);
+
+        return decoded;
+    },
+    async determineFilename(identifier: string): Promise<string> {
+        return basename(identifier + ".json");
+    },
+}
