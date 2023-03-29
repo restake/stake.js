acf6fc9f21ef7ab829ed9a6137f2fd4f4b1af7c2
Hans Oskar Aaviksoo <ha@restake.net>
2023-03-29 11:17:52 +0300

    Add data root construct

--- src/core/protocol/ethereum/EthereumSigner.ts
+++ src/core/protocol/ethereum/EthereumSigner.ts
@@ -1,8 +1,10 @@
 import { EthereumNetwork } from "./network.js";
 import { secp256k1Signer } from "../../signer/secp256k1Signer.js";
+import { keccak_256 } from "@noble/hashes/sha3";
 import { Transaction, SignedTransaction } from "./EthereumTransaction.js";
 import { TransactionSigner } from "../../signer/TransactionSigner.js";
 import { jsonrpc } from "../../utils/http.js";
+
 
 export type EthereumBlockResponse = {
     gasLimit: string;
@@ -54,6 +56,32 @@ export class EthereumSigner implements TransactionSigner<Transaction, SignedTran
             blockNumber,
             true,
         ]);
+    }
+
+    async getDataRoot(pubkey: string, withdrawal_credentials: string, amount: bigint, signature: string): Promise<Uint8Array> {
+
+        const pubkeyBytes = Buffer.from(pubkey.slice(2), 'hex');
+        const pubkeyRoot = keccak_256(pubkeyBytes);
+
+        const signatureBytes = Buffer.from(signature.slice(2), 'hex');
+        const signatureRoot = keccak_256(Buffer.concat([
+        keccak_256(signatureBytes.slice(0, 64)),
+        keccak_256(Buffer.concat([signatureBytes.slice(64), Buffer.alloc(32)]))
+        ]));
+
+        const withdrawalCredentialBytes = Buffer.from(withdrawal_credentials.slice(2), 'hex');
+        const amountBytes = Buffer.alloc(8);
+        amountBytes.writeBigUInt64BE(amount);
+
+        const depositDataBytes = Buffer.concat([
+        pubkeyRoot,
+        withdrawalCredentialBytes,
+        amountBytes,
+        signatureRoot
+        ]);
+        const depositDataRoot = keccak_256(depositDataBytes);
+
+        return depositDataRoot;
     }
 
     async getAddress(): Promise<string> {
