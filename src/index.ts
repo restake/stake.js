fd169e699bae4c83dae98e7578b675a72de6cc1f
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-03-30 19:02:37 +0300

    Add default export

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export * from "./protocol/interfaces/index.js";
 export * from "./service/network.js";
 export * from "./service/staking.js";
 export * from "./wallet/index.js";
+
+import { StakingService } from "./service/staking.js";
+export default StakingService;
