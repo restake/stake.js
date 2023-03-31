a3aed69090e1a7a36b4d0af6e616d573786a6bf0
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-03-31 15:42:30 +0300

    Fix deriveAddress implementation

--- src/core/protocol/avalanche/AvalancheSigner.ts
+++ src/core/protocol/avalanche/AvalancheSigner.ts
@@ -1,10 +1,12 @@
-import { AvalancheNetwork } from "./network.js";
+import { AvalancheChainID, AvalancheNetwork } from "./network.js";
 import { secp256k1Signer } from "../../signer/secp256k1Signer.js";
 import { Transaction, SignedTransaction } from "./AvalancheTransaction.js";
 import { TransactionSigner } from "../../signer/TransactionSigner.js";
 
 import { Avalanche, Buffer } from "avalanche";
 import { bech32 } from "bech32";
+import { sha256 } from "@noble/hashes/sha256";
+import { ripemd160 } from "@noble/hashes/ripemd160";
 
 export class AvalancheSigner implements TransactionSigner<Transaction, SignedTransaction>  {
     #parent: secp256k1Signer;
@@ -23,10 +25,15 @@ export class AvalancheSigner implements TransactionSigner<Transaction, SignedTra
     deriveAddress(chainID: string): string {
         const publicKey = this.#parent.publicKey;
         const networkID = this.#network.id;
-        return `${chainID}-${bech32.encode(networkID, publicKey.bytes)}`;
+
+        // The 33-byte compressed representation of the public key is hashed with sha256 once.
+        // The result is then hashed with ripemd160 to yield a 20-byte address.
+        const hash = ripemd160(sha256(publicKey.compressedBytes));
+        const words = bech32.toWords(hash);
+        return `${chainID}-${bech32.encode(networkID, words)}`;
     }
 
-    async signTransaction(transaction: Transaction): Promise <SignedTransaction>{
+    async signTransaction(transaction: Transaction): Promise<SignedTransaction> {
         const pKeyChain = this.#avalanche.PChain().keyChain();
         const signedTxn = transaction.payload.sign(pKeyChain);
 
