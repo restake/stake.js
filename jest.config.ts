728b8d6a11874742858cc9ac44baff7276612671
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-03-31 13:44:49 +0300

    Silence ts-jest version warning for now

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,7 @@
 import type { JestConfigWithTsJest } from "ts-jest";
 
+process.env["TS_JEST_DISABLE_VER_CHECKER"] = "true";
+
 export default <JestConfigWithTsJest> {
     preset: "ts-jest/presets/default-esm",
     extensionsToTreatAsEsm: [".ts"],
