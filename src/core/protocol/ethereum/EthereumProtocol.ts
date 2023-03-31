5f45d049a4458f2b88b52852546234494a86dea3
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-03-31 14:37:23 +0300

    Clean up EthereumProtocol

--- src/core/protocol/ethereum/EthereumProtocol.ts
+++ src/core/protocol/ethereum/EthereumProtocol.ts
@@ -1,15 +1,53 @@
-import { jsonrpc } from "../../utils/http.js";
+import { BlockFinality } from "./network.js";
 import { EthereumBlockResponse, EthereumSigner } from "./EthereumSigner.js";
+import { jsonrpc } from "../../utils/http.js";
 import { SignedTransaction, Transaction } from "./EthereumTransaction.js";
 import { TransactionBroadcaster } from "../../network/broadcaster.js";
-import { Transaction as EthTransaction } from "@ethereumjs/tx";
+
 import { Common }  from "@ethereumjs/common";
-import { ethers } from "ethers";
-import { Contract } from "ethers";
-import { BlockFinality } from "./network.js";
+import { ethers, Contract } from "ethers";
 import { hexToBytes } from "@noble/curves/abstract/utils";
+import { Transaction as EthTransaction, TxData } from "@ethereumjs/tx";
 
 export type EthereumBroadcastResponse = string;
+
+const depositContractAbi = [
+    {
+        "inputs": [
+            {
+                "internalType": "bytes",
+                "name": "pubkey",
+                "type": "bytes"
+            },
+            {
+                "internalType": "bytes",
+                "name": "withdrawal_credentials",
+                "type": "bytes"
+            },
+            {
+                "internalType": "bytes",
+                "name": "signature",
+                "type": "bytes"
+            },
+            {
+                "internalType": "bytes32",
+                "name": "data_root_value",
+                "type": "bytes32"
+            }
+        ],
+        "name": "deposit",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+] as const;
+
+// TODO: better name
+type Parameters = {
+    ethBlock: EthereumBlockResponse;
+    gasPrice: BigInt;
+    nonce: BigInt;
+};
 
 export class EthereumProtocol implements TransactionBroadcaster<SignedTransaction, EthereumBroadcastResponse> {
     static INSTANCE = new EthereumProtocol();
@@ -18,10 +56,11 @@ export class EthereumProtocol implements TransactionBroadcaster<SignedTransactio
         // no-op
     }
 
+    // TODO: better name
     private async fetchParameters(
         signer: EthereumSigner,
         block: BlockFinality | BigInt,
-    ): Promise<{ ethBlock: EthereumBlockResponse, gasPrice: BigInt, nonce: BigInt }> {
+    ): Promise<Parameters> {
         const senderAddress = signer.getAddress();
         const [ ethBlock, gasPrice ] = await Promise.all([ signer.fetchBlock(block), signer.fetchGasPrice() ]);
         const nonce = await signer.fetchNonce(senderAddress, typeof block === "bigint" ? block : BigInt(ethBlock.number));
@@ -33,34 +72,40 @@ export class EthereumProtocol implements TransactionBroadcaster<SignedTransactio
         };
     }
 
+    private constructTxData(parameters: Parameters): TxData {
+        const { ethBlock, gasPrice, nonce } = parameters;
+
+        return {
+            nonce: "0x" + nonce.toString(16),
+            gasPrice: "0x" + gasPrice.toString(16),
+            gasLimit: ethBlock.gasLimit,
+        };
+    }
+
+    private constructTransaction(signer: EthereumSigner, txData: TxData): EthTransaction {
+        return EthTransaction.fromTxData(txData, {
+            common: Common.custom({
+                name: signer.network.id,
+                networkId: signer.network.chainId,
+                chainId: signer.network.chainId,
+                url: signer.network.rpcUrl,
+            }),
+        });
+    }
+
     async transfer(
         signer: EthereumSigner,
         receiveAddress: string,
         amount: BigInt,
         block: BlockFinality | BigInt = "latest",
     ): Promise<Transaction>{
-        const chainId = signer.network.chainId;
-        const { ethBlock, gasPrice, nonce } = await this.fetchParameters(signer, block);
+        const parameters = await this.fetchParameters(signer, block);
 
-        const chainParams = {
-            name: 'testnetwork',
-            networkId: chainId,
-            chainId: chainId,
-            url: signer.network.rpcUrl,
-            comment: "Test Local Chain"
-        };
-
-        const customCommon = Common.custom(chainParams);
-
-        const txParams = {
-            nonce: "0x" + nonce.toString(16),
-            gasPrice: "0x" + gasPrice.toString(16),
-            gasLimit: ethBlock.gasLimit,
+        const payload = this.constructTransaction(signer, {
+            ...this.constructTxData(parameters),
             to: receiveAddress,
             value: "0x" + amount.toString(16),
-        }
-
-        const payload = EthTransaction.fromTxData(txParams, { common: customCommon });
+        });
 
         return {
             payload,
@@ -77,77 +122,32 @@ export class EthereumProtocol implements TransactionBroadcaster<SignedTransactio
         depositDataRoot: string,
         block: BlockFinality | BigInt = "latest",
     ): Promise<Transaction> {
-        const chainId = signer.network.chainId;
-        const { ethBlock, gasPrice, nonce } = await this.fetchParameters(signer, block);
+        const parameters = await this.fetchParameters(signer, block)
 
         const ethProvider = new ethers.providers.JsonRpcProvider(signer.network.rpcUrl);
 
-        const chainParams = {
-            name: signer.network.id,
-            networkId: chainId,
-            chainId: chainId,
-            url: signer.network.rpcUrl,
-        };
-        const customCommon = Common.custom(chainParams);
-
-        const depositAbi = [
-            {
-            "inputs": [
-                {
-                "internalType": "bytes",
-                "name": "pubkey",
-                "type": "bytes"
-                },
-                {
-                "internalType": "bytes",
-                "name": "withdrawal_credentials",
-                "type": "bytes"
-                },
-                {
-                "internalType": "bytes",
-                "name": "signature",
-                "type": "bytes"
-                },
-                {
-                "internalType": "bytes32",
-                "name": "data_root_value",
-                "type": "bytes32"
-                }
-            ],
-            "name": "deposit",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-            }
-        ];
-
         const contractAddress = "0x7479Fc54515ef6Ef1C649d54e91520F51ff77982";
-        const contract = new Contract(contractAddress, depositAbi, ethProvider);
-
+        const contract = new Contract(contractAddress, depositContractAbi, ethProvider);
 
         const validatorPublickeyBytes = hexToBytes(validatorPublickey.replace(/^0x/, ""));
         const withdrawalCredentialsBytes = hexToBytes(withdrawalCredentials.replace(/^0x/, ""));
         const validatorSignatureBytes = hexToBytes(validatorSignature.replace(/^0x/, ""));
         const depositDataRootBytes32 = ethers.utils.hexZeroPad(depositDataRoot, 32);
 
-        const txParams = {
-            nonce: "0x" + nonce.toString(16),
-            gasPrice: "0x" + gasPrice.toString(16),
-            gasLimit: ethBlock.gasLimit,
+        const payload = this.constructTransaction(signer, {
+            ...this.constructTxData(parameters),
             to: contractAddress,
             value: "0x" + amount.toString(16),
             data: contract.interface.encodeFunctionData(
-            "deposit",
-            [
-                validatorPublickeyBytes,
-                withdrawalCredentialsBytes,
-                validatorSignatureBytes,
-                depositDataRootBytes32,
-            ]
+                "deposit",
+                [
+                    validatorPublickeyBytes,
+                    withdrawalCredentialsBytes,
+                    validatorSignatureBytes,
+                    depositDataRootBytes32,
+                ]
             )
-        };
-
-        const payload = EthTransaction.fromTxData(txParams, { common: customCommon });
+        });
 
         return {
             payload,
