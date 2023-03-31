f772fe1710ea14cd9a2ccfd5e802dcd89d67381e
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-03-31 18:00:09 +0300

    Clean up decompression implementation

--- src/core/utils/secp256k1.ts
+++ src/core/utils/secp256k1.ts
@@ -1,43 +1,37 @@
-export function decompressSecp256k1PublicKey(compressedKey: string): string {
-    const prefix = compressedKey.slice(0,2);
-    const xValue = BigInt(`0x${compressedKey.slice(2)}`);
 
-    const params = BigInt("0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f");
-    const exponent = (params + BigInt(1)) / BigInt(4);
+import { secp256k1 } from "@noble/curves/secp256k1";
 
+const Fp = secp256k1.CURVE.Fp.ORDER;
+const exponent = (Fp + 1n) / 4n;
 
-    const yValuesSq = (xValue ** BigInt(3) + BigInt(7)) % params;
+export function decompressSecp256k1PublicKey(compressed: string): string {
+    const prefix = BigInt(`0x${compressed.slice(0, 2)}`);
+    const xValue = BigInt(`0x${compressed.slice(2)}`);
 
-    let yValue = modularExpo(yValuesSq, exponent, params);
-    console.log(yValuesSq);
+    const yValuesSq = (xValue ** 3n + 7n) % Fp;
+    let yValue = modularExpo(yValuesSq, exponent, Fp);
 
-    // Make sure that the correct y value is calculated based on the prefix (02 even, 03 odd)
-    if (prefix === "02" && yValue % BigInt(2) !== BigInt(0)) {
-        yValue = (params - yValue) % params;
+    // Make sure that the correct y value is calculated based on the prefix (2 even, 3 odd)
+    const prefixEven = prefix === 2n;
+    const yEven = yValue % 2n === 0n;
+    if (prefixEven !== yEven) {
+        yValue = (Fp - yValue) % Fp;
     }
-    if (prefix === "03" && yValue % BigInt(2) === BigInt(0)) {
-        yValue = (params - yValue) % params;
-    }
-
-    console.log(yValue);
 
     const xHex = xValue.toString(16).padStart(64, "0");
     const yHex = yValue.toString(16).padStart(64, "0");
-    const decompressed = `04${xHex}${yHex}`
-
-    return decompressed;
+    return `04${xHex}${yHex}`;
 }
 
 function modularExpo(x: bigint, y: bigint, p: bigint): bigint {
-    let result = BigInt(1)
-
-    while ( y > BigInt(0) ) {
-        if ( y % BigInt(2) === BigInt(1) ){
-            result = ( result * x ) % p;
+    let result = 1n;
+    while (y > 0n) {
+        if (y % 2n === 1n){
+            result = (result * x) % p;
         }
 
-        x = ( x * x ) % p;
-        y = y / BigInt(2);
+        x = (x ** 2n) % p;
+        y = y / 2n;
     }
 
     return result;
