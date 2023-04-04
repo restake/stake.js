55319863afe355f63cc871c7eb17e2c62de0e5db
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-04-04 18:55:51 +0300

    Update example

--- example/src/staking-code-golf.ts
+++ example/src/staking-code-golf.ts
@@ -1,2 +1,1 @@
-Promise.all([import("@restake/staking-sdk"),import("@restake/staking-sdk/wallet/filesystem")]).then(([{default:S},{default:F}])=>new S().near.stake(new F("key.json"),"restake.poolv1.near",50n))
-
+Promise.all([import("@restake/staking.js"),import("@restake/staking.js/wallet/filesystem")]).then(([{default:S},{default:F}])=>new S().near.stake(new F("key.json"),"restake.poolv1.near","0.1"));
