fb1d7741b0128d2e1a208f6eebd6a4eeff6ab623
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-04-04 18:50:45 +0300

    Update example

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,9 +1,8 @@
-import { StakingService, Wallet } from "@restake/staking-sdk";
-import { FilesystemWallet } from "@restake/staking-sdk/wallet/filesystem";
+import { StakingService, Wallet } from "@restake/staking.js";
+import { FilesystemWallet } from "@restake/staking.js/wallet/filesystem";
 
 const wallet: Wallet = new FilesystemWallet("key.json");
 
 const rstk = new StakingService("testnet");
-const amount0_1N = 100000000000000000000000n;
-const txId = await rstk.near.stake(wallet, "shurik.pool.f863973.m0", amount0_1N);
+const txId = await rstk.near.stake(wallet, "shurik.pool.f863973.m0", "0.1");
 console.log("txId", txId);
