7748e12b185e0e3d555b1c7ff6fbaf27b1cbde3a
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-04-04 18:44:40 +0300

    Make NEAR example default for now

--- example/src/index.ts
+++ example/src/index.ts
@@ -4,5 +4,6 @@ import { FilesystemWallet } from "@restake/staking-sdk/wallet/filesystem";
 const wallet: Wallet = new FilesystemWallet("key.json");
 
 const rstk = new StakingService("testnet");
-const txId = await rstk.near.stake(wallet, "restake.poolv1.near", 100000000000000000000000n);
+const amount0_1N = 100000000000000000000000n;
+const txId = await rstk.near.stake(wallet, "shurik.pool.f863973.m0", amount0_1N);
 console.log("txId", txId);
