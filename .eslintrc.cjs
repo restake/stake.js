4f435a7b15dc9097e90cb2c9c6631ecacba30ee2
Markkus Millend <mm@liangroup.io>
2023-04-11 15:44:11 +0200

    Add eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,10 @@
 module.exports = {
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
     root: true,
     env: {
         es2022: true,
-        node: true,
-    },
-    rules: {
-        "indent": ["error", 4, {
-            "MemberExpression": 1,
-        }],
-        "quotes": ["error", "double"],
-        "semi": ["error", "always"],
-        "object-curly-spacing": ["error", "always"],
-        "max-len": ["warn", 140],
-        "comma-dangle": ["error", "always-multiline"],
-        "arrow-parens": ["error", "always"],
-        "newline-before-return": "error",
-        "space-before-function-paren": ["error", {
-            "anonymous": "always",
-            "asyncArrow": "always",
-            "named": "never",
-        }],
-        "capitalized-comments": ["error", "always", {
-            "line": {
-                "ignorePattern": "pragma|ignored",
-            },
-            "block": {
-                "ignoreInlineComments": true,
-                "ignorePattern": "ignored",
-            },
-        }],
-        "@typescript-eslint/no-unused-vars": ["error", {
-            "argsIgnorePattern": "^_",
-        }],
-        "@typescript-eslint/no-explicit-any": "error",
-        "@typescript-eslint/no-inferrable-types": "off",
-    },
+        node: true
+    }
 };
