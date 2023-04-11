f08d77fadc0803d7cf70d30b88f12f51e1f0be6f
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-04-11 11:02:41 +0300

    Fix package name

--- example/src/staking-code-golf.ts
+++ example/src/staking-code-golf.ts
@@ -1,1 +1,1 @@
-Promise.all([import("@restake/staking.js"),import("@restake/staking.js/wallet/filesystem")]).then(([{default:S},{default:F}])=>new S().near.stake(new F("key.json"),"restake.poolv1.near","0.1"));
+Promise.all([import("@restake/stake.js"),import("@restake/stake.js/wallet/filesystem")]).then(([{default:S},{default:F}])=>new S().near.stake(new F("key.json"),"restake.poolv1.near","0.1"));
