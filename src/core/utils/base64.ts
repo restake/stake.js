59a6ba34bdc9cf4bb9ebbdb05f6666c7b08c7cff
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-04-12 12:15:24 +0300

    Add base64 url encoding support

--- src/core/utils/base64.ts
+++ src/core/utils/base64.ts
@@ -93,12 +93,14 @@ const base64abc = [
   "/",
 ];
 
+const base64abcUrl = base64abc.slice(0, base64abc.length - 2).concat(["-", "_"]);
+
 /**
  * CREDIT: https://gist.github.com/enepomnyaschih/72c423f727d395eeaa09697058238727
  * Encodes a given Uint8Array, ArrayBuffer or string into RFC4648 base64 representation
  * @param data
  */
-export function encode(data: ArrayBuffer | string): string {
+export function encode(data: ArrayBuffer | string, url: boolean = false): string {
   const uint8 = typeof data === "string"
     ? new TextEncoder().encode(data)
     : data instanceof Uint8Array
@@ -106,25 +108,32 @@ export function encode(data: ArrayBuffer | string): string {
     : new Uint8Array(data);
   let result = "",
     i;
+
+  const alphabet = url ? base64abcUrl : base64abc;
   const l = uint8.length;
   for (i = 2; i < l; i += 3) {
-    result += base64abc[uint8[i - 2] >> 2];
-    result += base64abc[((uint8[i - 2] & 0x03) << 4) | (uint8[i - 1] >> 4)];
-    result += base64abc[((uint8[i - 1] & 0x0f) << 2) | (uint8[i] >> 6)];
-    result += base64abc[uint8[i] & 0x3f];
+    result += alphabet[uint8[i - 2] >> 2];
+    result += alphabet[((uint8[i - 2] & 0x03) << 4) | (uint8[i - 1] >> 4)];
+    result += alphabet[((uint8[i - 1] & 0x0f) << 2) | (uint8[i] >> 6)];
+    result += alphabet[uint8[i] & 0x3f];
   }
   if (i === l + 1) {
     // 1 octet yet to write
-    result += base64abc[uint8[i - 2] >> 2];
-    result += base64abc[(uint8[i - 2] & 0x03) << 4];
+    result += alphabet[uint8[i - 2] >> 2];
+    result += alphabet[(uint8[i - 2] & 0x03) << 4];
+    if (!url) {
+      result += "==";
+    }
     result += "==";
   }
   if (i === l) {
     // 2 octets yet to write
-    result += base64abc[uint8[i - 2] >> 2];
-    result += base64abc[((uint8[i - 2] & 0x03) << 4) | (uint8[i - 1] >> 4)];
-    result += base64abc[(uint8[i - 1] & 0x0f) << 2];
-    result += "=";
+    result += alphabet[uint8[i - 2] >> 2];
+    result += alphabet[((uint8[i - 2] & 0x03) << 4) | (uint8[i - 1] >> 4)];
+    result += alphabet[(uint8[i - 1] & 0x0f) << 2];
+    if (!url) {
+      result += "=";
+    }
   }
   return result;
 }
@@ -133,7 +142,7 @@ export function encode(data: ArrayBuffer | string): string {
  * Decodes a given RFC4648 base64 encoded string
  * @param b64
  */
-export function decode(b64: string): Uint8Array {
+export function decode(b64: string, url: boolean = false): Uint8Array {
   const binString = atob(b64);
   const size = binString.length;
   const bytes = new Uint8Array(size);
