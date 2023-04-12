483b4bf1123224b814731c9b3c23eb9939505b2a
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-04-12 23:37:05 +0300

    Continue implementing Fireblocks signer provider

--- src/core/signer/provider/FireblocksSignerProvider.ts
+++ src/core/signer/provider/FireblocksSignerProvider.ts
@@ -1,9 +1,23 @@
-import { KeyType, PublicKey, Signer } from "../index.js";
-import { importKey } from "./fireblocks/fetch.js";
+import { ZodType, z } from "zod";
+import { KeyType, PublicKey, Signer, ed25519PublicKey } from "../index.js";
+import { fireblocksAPI, importKey } from "./fireblocks/fetch.js";
+import { Account, Address, PublicKeyInfo, Transaction } from "./fireblocks/types.js";
 import { SignerProvider } from "./provider.js";
+import { hexToBytes } from "@noble/curves/abstract/utils";
+
+
+export type FireblocksKeyAlgorithm = "MPC_EDDSA_ED25519" | "MPC_ECDSA_SECP256K1";
+
+export const keyTypeToFireblocksAlgorithm = {
+    "ed25519": "MPC_EDDSA_ED25519",
+    "secp256k1": "MPC_ECDSA_SECP256K1",
+} as const;
 
 export interface FireblocksSignerOptions {
-    expectedAlgorithm?: "MPC_EDDSA_ED25519" | string;
+    assetId: string;
+    // XXX: no idea what this is for honestly
+    change?: number;
+    expectedAlgorithm: FireblocksKeyAlgorithm;
 }
 
 export class FireblocksSignerProvider<S extends Signer<K>, K extends KeyType> implements SignerProvider<S, K, FireblocksSignerOptions> {
@@ -17,22 +31,208 @@ export class FireblocksSignerProvider<S extends Signer<K>, K extends KeyType> im
         this.#apiBaseUrl = apiBaseUrl;
     }
 
-    async getSigner(_identifier: string, _options: FireblocksSignerOptions): Promise<S> {
+    async getSigner(vaultAccountId: string, options: FireblocksSignerOptions): Promise<S> {
         const secretKey = await importKey(this.#apiSecret);
+        const signer = new FireblocksSigner(
+            "ed25519",
+            vaultAccountId,
+            this.#apiKey,
+            secretKey,
+            this.#apiBaseUrl,
+            options,
+        );
+        await signer.init();
 
-        throw new Error("Method not implemented");
+        // TODO
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        return signer;
     }
 }
 
-class FireblocksSigner<K> implements Signer<K> {
-    keyType: K;
-    publicKey: PublicKey<K>;
+class FireblocksSigner<K extends KeyType> implements Signer<K> {
+    #vaultAccountId: string;
+    #apiKey: string;
+    #secretKey: CryptoKey;
+    #apiBaseUrl: string;
+    #assetId: string;
+    #expectedAlgorithm: FireblocksKeyAlgorithm;
 
-    sign(payload: Uint8Array): Promise<Uint8Array> {
-        throw new Error("Method not implemented.");
+    #_publicKey: PublicKey<K> | undefined;
+    keyType: K;
+
+    static txFailStatuses = new Set<Transaction["status"]>([
+        "BLOCKED",
+        "CANCELLED",
+        "FAILED",
+    ]);
+
+    constructor(
+        keyType: K,
+        vaultAccountId: string,
+        apiKey: string,
+        secretKey: CryptoKey,
+        apiBaseUrl: string,
+
+        options: FireblocksSignerOptions,
+    ) {
+        this.keyType = keyType;
+        this.#vaultAccountId = vaultAccountId;
+        this.#apiKey = apiKey;
+        this.#secretKey = secretKey;
+        this.#apiBaseUrl = apiBaseUrl;
+
+        this.#assetId = options.assetId;
+        this.#expectedAlgorithm = options.expectedAlgorithm;
     }
 
-    verify(payload: Uint8Array, signature: Uint8Array): Promise<boolean> {
-        throw new Error("Method not implemented.");
+    async init(): Promise<void> {
+        // Ensure that account exists
+        let account: Account;
+        try {
+            account = await this.fireblocksGet(
+                Account,
+                `/v1/vault/accounts/${this.#vaultAccountId}`,
+            );
+
+            account.id;
+        } catch (err) {
+            throw new Error(`Vault account "${this.#vaultAccountId}" does not exist`, { cause: err as Error });
+        }
+
+        // Ensure we have at least one address available for desired asset
+        // TODO: do we need `compressed=true` here?
+        const addresses = await this.fireblocksGet(
+            z.array(Address).nonempty(),
+            `/v1/vault/accounts/${this.#vaultAccountId}/${this.#assetId}/addresses?compressed=true`,
+        );
+        if (addresses.length < 1) {
+            throw new Error(`No addresses available for asset "${this.#assetId}"`);
+        }
+
+        // Query public key
+        const change = 0;
+        const addressIdx = 0;
+        const publicKeyInfo = await this.fireblocksGet(
+            PublicKeyInfo,
+            `/v1/vault/accounts/${this.#vaultAccountId}/${this.#assetId}/${change}/${addressIdx}/public_key_info`,
+        );
+
+        // Ensure that public key is using expected algorithm
+        if (publicKeyInfo.algorithm !== this.#expectedAlgorithm) {
+            throw new Error(`Expected "${this.#expectedAlgorithm}", got "${publicKeyInfo.algorithm}"`);
+        }
+
+        const publicKeyBytes = hexToBytes(publicKeyInfo.publicKey);
+
+        // TODO: unsafe cast
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        this.#_publicKey = new ed25519PublicKey(publicKeyBytes) as PublicKey<K>;
+
+        return;
+    }
+
+    get publicKey(): PublicKey<K> {
+        const p = this.#_publicKey;
+        if (!p) {
+            throw new Error("Illegal state");
+        }
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        return p;
+    }
+
+    private async _sign(payload: Uint8Array): Promise<Transaction["signedMessages"][number]["signature"]> {
+        const { id } = await this.fireblocksPost(z.object({ id: z.string() }), "/v1/transactions", {
+            assetId: this.#assetId,
+            source: {
+                type: "VAULT_ACCOUNT",
+                id: this.#vaultAccountId,
+            },
+            operation: "RAW",
+            // Old - note: "Restake stake.js",
+            extraParameters: {
+                rawMessageData: {
+                    messages: [
+                        {
+                            content: payload,
+                        },
+                    ],
+                },
+            },
+        });
+
+        // Wait until transaction status is completed
+        await this.waitTransaction(id);
+
+        // Fetch previously created transaction
+        const transaction = await this.fireblocksGet(Transaction, `/v1/transactions/${id}`);
+        if (transaction.signedMessages.length < 1) {
+            throw new Error("No signature available");
+        }
+
+        const signature = transaction.signedMessages[0].signature;
+
+        return signature;
+    }
+
+    async sign(payload: Uint8Array): Promise<Uint8Array> {
+        const signature = await this._sign(payload);
+
+        return hexToBytes(signature.fullSig);
+    }
+
+    async edSign(payload: Uint8Array): Promise<{ r: bigint, s: bigint, recovery?: number }> {
+        const signature = await this._sign(payload);
+
+        return {
+            r: BigInt(signature.r),
+            s: BigInt(signature.s),
+            // TODO: v is Ethereum-specific.
+            recovery: Number(signature.v),
+        };
+    }
+
+    verify(_payload: Uint8Array, _signature: Uint8Array): Promise<boolean> {
+        throw new Error("verify not implemented");
+    }
+
+    private async fireblocksGet<T extends ZodType, R extends z.infer<T>>(
+        validator: T,
+        endpoint: string,
+    ): Promise<R> {
+        return fireblocksAPI(validator, this.#apiKey, this.#secretKey, this.#apiBaseUrl, "GET", endpoint);
+    }
+
+    private async fireblocksPost<T extends ZodType, R extends z.infer<T>>(
+        validator: T,
+        endpoint: string,
+        body: unknown,
+    ): Promise<R> {
+        return fireblocksAPI(validator, this.#apiKey, this.#secretKey, this.#apiBaseUrl, "POST", endpoint, body);
+    }
+
+    private async getTransaction(txId: string): Promise<Transaction> {
+        return await this.fireblocksGet(Transaction, `/v1/transactions/${txId}`);
+    }
+
+    private async waitTransaction(txId: string): Promise<void> {
+        // TODO: copied from fireblocks NEAR reference SDK, surely there's a better way for this...
+        let tx: Transaction = await this.getTransaction(txId);
+
+        while (tx.status !== "COMPLETED") {
+            // Sleep a bit
+            await new Promise<void>((res) => setTimeout(() => res(), 5000));
+
+            // Retry
+            tx = await this.getTransaction(txId);
+            if (FireblocksSigner.txFailStatuses.has(tx.status)) {
+                throw new Error(`Transaction ${txId} failed: "${tx.status}" (sub=${tx.subStatus})`);
+            }
+        }
+
+        return;
     }
 }
--- src/core/signer/provider/fireblocks/types.ts
+++ src/core/signer/provider/fireblocks/types.ts
@@ -1,5 +1,21 @@
 import { z } from "zod";
 
+export const Address = z.object({
+    assetId: z.string(),
+    address: z.string(),
+    description: z.string(),
+    tag: z.string(),
+    type: z.string(),
+    customerRefId: z.optional(z.string()),
+    addressFormat: z.optional(z.string()),
+    legacyAddress: z.string(),
+    enterpriseAddress: z.string(),
+    bip44AddressIndex: z.optional(z.number()),
+    userDefined: z.boolean(),
+});
+
+export type Address = z.infer<typeof Address>;
+
 export const Asset = z.object({
     id: z.string(),
 });
@@ -13,3 +29,46 @@ export const Account = z.object({
 });
 
 export type Account = z.infer<typeof Account>;
+
+export const PublicKeyInfo = z.object({
+    algorithm: z.string(),
+    derivationPath: z.array(z.number()),
+    // Encoded as hex
+    publicKey: z.string(),
+});
+
+export type PublicKeyInfo = z.infer<typeof PublicKeyInfo>;
+
+export const Transaction = z.object({
+    id: z.string(),
+    status: z.enum([
+        "BLOCKED",
+        "BROADCASTING",
+        "CANCELLED",
+        "COMPLETED",
+        "CONFIRMING",
+        "FAILED",
+        "PARTIALLY_COMPLETED",
+        "PENDING_3RD_PARTY",
+        "PENDING_3RD_PARTY_MANUAL_APPROVAL",
+        "PENDING_AML_SCREENING",
+        "PENDING_AUTHORIZATION",
+        "PENDING_SIGNATURE",
+        "QUEUED",
+        "REJECTED",
+        "SUBMITTED",
+    ]),
+    subStatus: z.string(),
+    signedMessages: z.array(z.object({
+        algorithm: z.string(),
+        publicKey: z.string(),
+        signature: z.object({
+            fullSig: z.string(),
+            r: z.string(),
+            s: z.string(),
+            v: z.string(),
+        }),
+    })),
+});
+
+export type Transaction = z.infer<typeof Transaction>;
