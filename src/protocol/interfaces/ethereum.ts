4ac24e2e105920eb873911a84e4b554969ef6b7c
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-04-12 10:45:06 +0300

    Implement high level stake wrapper for wallet
    This is mainly useful for wallet providers which do not support raw transaction signing

--- src/protocol/interfaces/ethereum.ts
+++ src/protocol/interfaces/ethereum.ts
@@ -3,3 +3,10 @@ import { Wallet } from "../../wallet/index.js";
 export interface EthereumStakingProtocol {
     stake(wallet: Wallet): Promise<string>;
 }
+
+export interface EthereumStakeParameters {
+    amount: string;
+    withdrawalCredentials: string;
+    validatorSignature: string;
+    depositDataRoot: string;
+}
