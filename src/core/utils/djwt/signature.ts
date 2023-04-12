1ee766a05008054d71b3ac9c558e29f4d442f20c
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-04-12 12:15:10 +0300

    Adapt djwt code to work in this environment

--- src/core/utils/djwt/signature.ts
+++ src/core/utils/djwt/signature.ts
@@ -1,35 +1,35 @@
-import { getAlgorithm } from "./algorithm.ts";
-import { base64url } from "./deps.ts";
-import { encoder, isNull } from "./util.ts";
-
-import type { Algorithm } from "./algorithm.ts";
+import { getAlgorithm } from "./algorithm.js";
+import { encoder, isNull } from "./util.js";
+import type { Algorithm } from "./algorithm.js";
+import { encode as b64encode } from "../base64.js";
 
 export async function verify(
-  signature: Uint8Array,
-  key: CryptoKey | null,
-  alg: Algorithm,
-  signingInput: string,
+    signature: Uint8Array,
+    key: CryptoKey | null,
+    alg: Algorithm,
+    signingInput: string,
 ): Promise<boolean> {
-  return isNull(key) ? signature.length === 0 : await crypto.subtle.verify(
-    getAlgorithm(alg),
-    key,
-    signature,
-    encoder.encode(signingInput),
-  );
+    return isNull(key) ? signature.length === 0 : await crypto.subtle.verify(
+        getAlgorithm(alg),
+        key,
+        signature,
+        encoder.encode(signingInput),
+    );
 }
 
 export async function create(
-  alg: Algorithm,
-  key: CryptoKey | null,
-  signingInput: string,
+    alg: Algorithm,
+    key: CryptoKey | null,
+    signingInput: string,
 ): Promise<string> {
-  return isNull(key) ? "" : base64url.encode(
-    new Uint8Array(
-      await crypto.subtle.sign(
-        getAlgorithm(alg),
-        key,
-        encoder.encode(signingInput),
-      ),
-    ),
-  );
+    return isNull(key) ? "" : b64encode(
+        new Uint8Array(
+            await crypto.subtle.sign(
+                getAlgorithm(alg),
+                key,
+                encoder.encode(signingInput),
+            ),
+        ),
+        true,
+    );
 }
--- src/core/utils/djwt/tests/test_deps.ts
+++
@@ -1,9 +0,0 @@
-export {
-  assertEquals,
-  assertRejects,
-  assertThrows,
-} from "https://deno.land/std@0.161.0/testing/asserts.ts";
-
-export {
-  decode as decodeHex,
-} from "https://deno.land/std@0.161.0/encoding/hex.ts";
