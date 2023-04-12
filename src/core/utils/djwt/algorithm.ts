08a93244de743f26ee5f373cf8781827d532baa8
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-04-12 12:15:10 +0300

    Adapt djwt code to work in this environment

--- src/core/utils/djwt/algorithm.ts
+++ src/core/utils/djwt/algorithm.ts
@@ -1,4 +1,4 @@
-import { isNotNull, isString } from "./util.ts";
+import { isNotNull, isString } from "./util.js";
 
 /*
  * JSW §1: Cryptographic algorithms and identifiers for use with this specification
@@ -24,78 +24,79 @@ export type Algorithm =
 // Still needs an 'any' type! Does anyone have an idea?
 // https://github.com/denoland/deno/blob/main/ext/crypto/lib.deno_crypto.d.ts
 function isHashedKeyAlgorithm(
-  // deno-lint-ignore no-explicit-any
-  algorithm: Record<string, any>,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    algorithm: Record<string, any>,
 ): algorithm is HmacKeyAlgorithm | RsaHashedKeyAlgorithm {
-  return isString(algorithm.hash?.name);
+    return isString(algorithm.hash?.name);
 }
 
 function isEcKeyAlgorithm(
-  // deno-lint-ignore no-explicit-any
-  algorithm: Record<string, any>,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    algorithm: Record<string, any>,
 ): algorithm is EcKeyAlgorithm {
-  return isString(algorithm.namedCurve);
+    return isString(algorithm.namedCurve);
 }
 
 export function verify(alg: Algorithm, key: CryptoKey | null): boolean {
-  if (alg === "none") {
-    if (isNotNull(key)) {
-      throw new Error(`The alg '${alg}' does not allow a key.`);
-    } else return true;
-  } else {
-    if (!key) throw new Error(`The alg '${alg}' demands a key.`);
-    const keyAlgorithm = key.algorithm;
-    const algAlgorithm = getAlgorithm(alg);
-    if (keyAlgorithm.name === algAlgorithm.name) {
-      if (isHashedKeyAlgorithm(keyAlgorithm)) {
-        return keyAlgorithm.hash.name === algAlgorithm.hash.name;
-      } else if (isEcKeyAlgorithm(keyAlgorithm)) {
-        return keyAlgorithm.namedCurve === algAlgorithm.namedCurve;
-      }
+    if (alg === "none") {
+        if (isNotNull(key)) {
+            throw new Error(`The alg '${alg}' does not allow a key.`);
+        } else return true;
+    } else {
+        if (!key) throw new Error(`The alg '${alg}' demands a key.`);
+        const keyAlgorithm = key.algorithm;
+        const algAlgorithm = getAlgorithm(alg);
+        if (keyAlgorithm.name === algAlgorithm.name) {
+            if (isHashedKeyAlgorithm(keyAlgorithm)) {
+                return keyAlgorithm.hash.name === algAlgorithm.hash.name;
+            } else if (isEcKeyAlgorithm(keyAlgorithm)) {
+                return keyAlgorithm.namedCurve === algAlgorithm.namedCurve;
+            }
+        }
+
+        return false;
     }
-    return false;
-  }
 }
 
 export function getAlgorithm(alg: Algorithm) {
-  switch (alg) {
+    switch (alg) {
     case "HS256":
-      return { hash: { name: "SHA-256" }, name: "HMAC" };
+        return { hash: { name: "SHA-256" }, name: "HMAC" };
     case "HS384":
-      return { hash: { name: "SHA-384" }, name: "HMAC" };
+        return { hash: { name: "SHA-384" }, name: "HMAC" };
     case "HS512":
-      return { hash: { name: "SHA-512" }, name: "HMAC" };
+        return { hash: { name: "SHA-512" }, name: "HMAC" };
     case "PS256":
-      return {
-        hash: { name: "SHA-256" },
-        name: "RSA-PSS",
-        saltLength: 256 >> 3,
-      };
+        return {
+            hash: { name: "SHA-256" },
+            name: "RSA-PSS",
+            saltLength: 256 >> 3,
+        };
     case "PS384":
-      return {
-        hash: { name: "SHA-384" },
-        name: "RSA-PSS",
-        saltLength: 384 >> 3,
-      };
+        return {
+            hash: { name: "SHA-384" },
+            name: "RSA-PSS",
+            saltLength: 384 >> 3,
+        };
     case "PS512":
-      return {
-        hash: { name: "SHA-512" },
-        name: "RSA-PSS",
-        saltLength: 512 >> 3,
-      };
+        return {
+            hash: { name: "SHA-512" },
+            name: "RSA-PSS",
+            saltLength: 512 >> 3,
+        };
     case "RS256":
-      return { hash: { name: "SHA-256" }, name: "RSASSA-PKCS1-v1_5" };
+        return { hash: { name: "SHA-256" }, name: "RSASSA-PKCS1-v1_5" };
     case "RS384":
-      return { hash: { name: "SHA-384" }, name: "RSASSA-PKCS1-v1_5" };
+        return { hash: { name: "SHA-384" }, name: "RSASSA-PKCS1-v1_5" };
     case "RS512":
-      return { hash: { name: "SHA-512" }, name: "RSASSA-PKCS1-v1_5" };
+        return { hash: { name: "SHA-512" }, name: "RSASSA-PKCS1-v1_5" };
     case "ES256":
-      return { hash: { name: "SHA-256" }, name: "ECDSA", namedCurve: "P-256" };
+        return { hash: { name: "SHA-256" }, name: "ECDSA", namedCurve: "P-256" };
     case "ES384":
-      return { hash: { name: "SHA-384" }, name: "ECDSA", namedCurve: "P-384" };
-    // case "ES512":
-    // return { hash: { name: "SHA-512" }, name: "ECDSA", namedCurve: "P-521" };
+        return { hash: { name: "SHA-384" }, name: "ECDSA", namedCurve: "P-384" };
+    // Case "ES512":
+    // Return { hash: { name: "SHA-512" }, name: "ECDSA", namedCurve: "P-521" };
     default:
-      throw new Error(`The jwt's alg '${alg}' is not supported.`);
-  }
+        throw new Error(`The jwt's alg '${alg}' is not supported.`);
+    }
 }
