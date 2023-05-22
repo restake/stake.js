0d3bf47a46c9f4e5d9a5790e4341cd40246f5ba5
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-05-23 02:51:01 +0300

    Create signSync method

--- src/core/signer/signer.ts
+++ src/core/signer/signer.ts
@@ -13,6 +13,15 @@ export interface Signer<K extends KeyType> {
     sign(payload: Uint8Array): Promise<Uint8Array>;
 
     /**
+     * Signs provided payload synchronously. If synchronous signing is not supported,
+     * then this method throws an error
+     *
+     * @param payload Data to sign
+     * @returns Signature
+     */
+    signSync(payload: Uint8Array): Uint8Array;
+
+    /**
      * Verifies whether signature is valid
      *
      * @param payload Payload with a signature
