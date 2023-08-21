0b42081f679b8253fd96bf5c3deb199be0f5f920
Maxime Rutagarama <mr@liangroup.io>
2023-08-21 13:46:01 +0200

    RSW-112: Implement Near Protocol Service

---
+++ src/protocols/near/service.ts
@@ -0,0 +1,37 @@
+import { ProtocolService } from "../../services/index.ts";
+import { SignerWallet } from "../../wallets/index.ts";
+import { NearProtocolTransactionEngine } from "./transactionEngine.ts";
+import { NearProtocolNetwork } from "./types.ts";
+
+export class NearProtocolService extends ProtocolService<NearProtocolTransactionEngine> {
+    tx: NearProtocolTransactionEngine;
+
+    constructor(network: NearProtocolNetwork = "mainnet", rpcUrl?: URL) {
+        super();
+        this.tx = new NearProtocolTransactionEngine(network, rpcUrl);
+    }
+
+    async stake(wallet: SignerWallet, validator: string, amount: number): Promise<string> {
+        const rawTx = await this.tx.buildStakeTx(wallet, validator, amount);
+        const signedTx = await this.tx.sign(wallet, rawTx);
+        const txId = await this.tx.broadcast(signedTx);
+
+        return txId;
+    }
+
+    async withdraw(wallet: SignerWallet, validator: string, amount: number | "all"): Promise<string> {
+        const rawTx = await this.tx.buildWithdrawTx(wallet, validator, amount);
+        const signedTx = await this.tx.sign(wallet, rawTx);
+        const txId = await this.tx.broadcast(signedTx);
+
+        return txId;
+    }
+
+    async unstake(wallet: SignerWallet, validator: string, amount: number | "all"): Promise<string> {
+        const rawTx = await this.tx.buildUnstakeTx(wallet, validator, amount);
+        const signedTx = await this.tx.sign(wallet, rawTx);
+        const txId = await this.tx.broadcast(signedTx);
+
+        return txId;
+    }
+}
