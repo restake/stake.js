45cd0735818b94c201865e3e350f5d543cd70615
Maxime Rutagarama <mr@liangroup.io>
2024-02-11 20:10:07 +0100

    Format transaction before signing and sending in near wallet selector

--- packages/wallets/near-selector/src/index.ts
+++ packages/wallets/near-selector/src/index.ts
@@ -1,5 +1,6 @@
-import { NetworkConfig, Signature, SignerWallet, RawTransaction, Protocol, PROTOCOL, NearProtocol } from "@restake/stake.js-core";
-import { WalletSelector } from "@near-wallet-selector/core";
+import { NetworkConfig, Signature, SignerWallet, Protocol, PROTOCOL } from "@restake/stake.js-core";
+import { NearProtocolRawTransaction } from "@restake/stake.js-near-protocol";
+import { WalletSelector, FunctionCallAction } from "@near-wallet-selector/core";
 
 export class NearSelectorWallet implements SignerWallet {
     walletSelector;
@@ -13,10 +14,22 @@ export class NearSelectorWallet implements SignerWallet {
             throw new Error("NearSelectorWallet only supports Near Protocol.");
         }
 
-        const accounts = await this.walletSelector.wallet.getAccounts();
-        const account = accounts.find((account: { accountId: string }) => account.accountId === selector);
+        const wallet = await this.walletSelector.wallet();
+        const accounts = await wallet.getAccounts();
 
-        return account.accountId || account.publicKey;
+        if (!selector) {
+            const account = accounts[0];
+
+            return account.accountId || account.publicKey;
+        } else {
+            const account = accounts.find((account: { accountId: string }) => account.accountId === selector);
+
+            if (!account) {
+                throw new Error(`Account with selector ${selector} not found.`);
+            }
+
+            return account.accountId || account.publicKey;
+        }
     }
 
     async getPublicKey<P extends Protocol>(networkConfig: NetworkConfig<P>, selector?: string): Promise<string> {
@@ -24,10 +37,22 @@ export class NearSelectorWallet implements SignerWallet {
             throw new Error("NearSelectorWallet only supports Near Protocol.");
         }
 
-        const accounts = await this.walletSelector.wallet.getAccounts();
-        const account = accounts.find((account: { accountId: string }) => account.accountId === selector);
+        const wallet = await this.walletSelector.wallet();
+        const accounts = await wallet.getAccounts();
 
-        return account.publicKey;
+        if (!selector) {
+            const account = accounts[0];
+
+            return account.accountId || account.publicKey;
+        } else {
+            const account = accounts.find((account: { accountId: string }) => account.accountId === selector);
+
+            if (!account) {
+                throw new Error(`Account with selector ${selector} not found.`);
+            }
+
+            return account.accountId || account.publicKey;
+        }
     }
 
     async sign(): Promise<Signature> {
@@ -35,8 +60,38 @@ export class NearSelectorWallet implements SignerWallet {
     }
 
     async signAndBroadcast(
-        rawTx: RawTransaction<NearProtocol>,
+        rawTx: NearProtocolRawTransaction,
     ): Promise<void> {
-        this.walletSelector.wallet.signAndSendTransactions({ transactions: [rawTx] });
+
+        const transaction = rawTx.transaction;
+
+        const transformed: { signerId: string, receiverId: string, actions: FunctionCallAction[] } = {
+            signerId: transaction.signerId,
+            receiverId: transaction.receiverId,
+            actions: [],
+        };
+
+        transaction.actions.forEach((action) => {
+            if (action.functionCall) {
+                // Decode the Uint8Array args to a string
+                const argsString = new TextDecoder().decode(action.functionCall.args);
+                const argsObject = JSON.parse(argsString);
+
+                const transformedAction = {
+                    type: "FunctionCall",
+                    params: {
+                        methodName: action.functionCall.methodName,
+                        args: argsObject,
+                        gas: action.functionCall.gas.toString(), // Convert BN to string
+                        deposit: action.functionCall.deposit.toString(), // Convert BN to string
+                    },
+                };
+
+                transformed.actions.push(transformedAction);
+            }
+        });
+
+        const wallet = await this.walletSelector.wallet();
+        await wallet.signAndSendTransactions({ transactions: [transformed] });
     }
 }
